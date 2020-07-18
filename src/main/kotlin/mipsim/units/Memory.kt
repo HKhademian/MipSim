67f77a21a5c6f6aab5ce382d343507a3bf331bb1
Hossain Khademian <hossain.khademian@gmail.com>
2020-07-18 15:32:49 +0430

    Memory
    add Memory class
    define bytes and words to work with them

--- src/main/kotlin/mipsim/units/Memory.kt
+++ src/main/kotlin/mipsim/units/Memory.kt
@@ -2,6 +2,12 @@ package mipsim.units
 
 import sim.base.*
 
+/// each byte is 8 bit of data
+const val BYTE_SIZE = 8
+
+/// each word is 4 byte of data
+const val WORD_SIZE = 4 * BYTE_SIZE
+
 /**
  *  a memory bit is a simple D-FF to store values
  *  to store a value on next clock edge, just use `MemBit`.`set`
@@ -22,12 +28,22 @@ class MemBit : Element, MutableValue {
 		next.const().also { curr.set(it) }
 }
 
-/** literally, it's 8 MemBit */
-class MemByte private constructor(private val bits: List<MemBit>) : List<Value> by bits {
-	constructor() : this((0 until 8).map { MemBit() })
+/**
+ *  a memory is just a list of mem-bits,
+ *  InstructionMemory and DataMemory is not a simple memory,
+ *  but they use memory to stores data
+ */
+class Memory private constructor(private val bits: List<MemBit>) : List<MemBit> by bits {
+	internal constructor(bitCount: Int) : this((0 until bitCount).map { MemBit() })
 }
 
-/** literally, it's 4 MemByte */
-class MemWord private constructor(private val bytes: List<MemByte>) : List<MemByte> by bytes {
-	constructor() : this((0 until 8).map { MemByte() })
-}
+inline val List<Value>.bitCount: Int get() = this.size
+inline val List<Value>.byteCount: Int get() = this.bitCount / BYTE_SIZE
+inline val List<Value>.wordCount: Int get() = this.bitCount / WORD_SIZE
+
+fun List<Value>.getByte(i: Int): List<Value> = this.subList(BYTE_SIZE * i, BYTE_SIZE * i + BYTE_SIZE)
+fun List<Value>.getWord(i: Int): List<Value> = this.subList(WORD_SIZE * i, WORD_SIZE * i + WORD_SIZE)
+
+fun createMemory(n: Int = 1) = Memory(n)
+fun createBytes(n: Int = 1) = createMemory(n * BYTE_SIZE)
+fun createWords(n: Int = 1) = createMemory(n * WORD_SIZE)
