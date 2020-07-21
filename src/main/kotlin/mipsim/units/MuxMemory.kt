eb50c6229a83a85a9cd8cbc7e7a5edc227806dda
Hossain Khademian <hossain.khademian@gmail.com>
2020-07-21 23:16:04 +0430

    MuxMemory
    an implementation of Memories which use mux to select requested word address

---
+++ src/main/kotlin/mipsim/units/MuxMemory.kt
@@ -0,0 +1,14 @@
+package mipsim.units
+
+import sim.base.Bus
+import sim.base.MutableBus
+import sim.base.bus
+import sim.real.mux
+
+open class MuxMemory protected constructor(addressBusSize: Int) {
+	protected val wordCount: Int = 1 shl addressBusSize
+	protected val memory: Memory = createWords(wordCount)
+	protected val words: List<MutableBus> = (0 until wordCount).map { memory.getWord(it) }
+	val address: MutableBus = bus(wordCount)
+	val word: Bus = mux(address, words)
+}
