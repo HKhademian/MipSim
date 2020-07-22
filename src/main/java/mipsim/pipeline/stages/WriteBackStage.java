817f9d3466b2a15d4f6dcf2c00fb0083367872fb
mehdi <mf1378mf@yahoo.com>
2020-07-23 03:08:16 +0430

    I Write test for Write Back data but  there is some problem for me

--- src/main/java/mipsim/pipeline/stages/WriteBackStage.java
+++ src/main/java/mipsim/pipeline/stages/WriteBackStage.java
@@ -2,7 +2,11 @@ package mipsim.pipeline.stages;
 
 import mipsim.Processor;
 import sim.base.BusKt;
+import sim.base.ValueKt;
 import sim.real.MuxKt;
+import sim.test.TestKt;
+
+import static mipsim.sim.InstructionParserKt.parseInstructionToBin;
 
 public class WriteBackStage extends Stage {
 	public WriteBackStage(final Processor processor) {
@@ -25,6 +29,49 @@ public class WriteBackStage extends Stage {
 	public void eval() {
 
 	}
+	/**
+	 * test by Mehdi Fe
+	 */
+	//todo check it with help of hossain :)  
+	public static void main(final String... args) {
+		final var processor = new Processor();
+		final var MemWbReg = processor.memwb;
+		final var regFile = processor.registerFile;
+		processor.wbStage.init();
+		TestKt.testOn(processor.registerFile, "Don't write anything", () -> {
+
+			BusKt.set(MemWbReg.aluData,BusKt.toBus(4005));
+			BusKt.set(MemWbReg.memoryData,BusKt.toBus(12044));
+			BusKt.set(MemWbReg.rdRegister,BusKt.toBus(10));//write in $s9
+			MemWbReg.regWrite = ValueKt.mut(false);
+			MemWbReg.memToReg = ValueKt.mut(true);
+			MemWbReg.eval();
+			regFile.eval();
+		});
 
 
-}
+		TestKt.testOn(processor.registerFile, "write something from memory", () -> {
+			BusKt.set(MemWbReg.aluData,BusKt.toBus(4005));
+			BusKt.set(MemWbReg.memoryData,BusKt.toBus(12044));
+			BusKt.set(MemWbReg.rdRegister,BusKt.toBus(10));//write in $s9
+			MemWbReg.regWrite = ValueKt.mut(true);
+			MemWbReg.memToReg = ValueKt.mut(true);
+			MemWbReg.eval();
+			regFile.eval();
+		});
+
+		TestKt.testOn(processor.registerFile, "write something from aluResult", () -> {
+			BusKt.set(MemWbReg.aluData,BusKt.toBus(4005));
+			BusKt.set(MemWbReg.memoryData,BusKt.toBus(12044));
+			BusKt.set(MemWbReg.rdRegister,BusKt.toBus(10));//write in $s9
+			MemWbReg.regWrite = ValueKt.mut(true);
+			MemWbReg.memToReg = ValueKt.mut(false);
+			MemWbReg.eval();
+			regFile.eval();
+		});
+
+
+
+	}
+
+	}
