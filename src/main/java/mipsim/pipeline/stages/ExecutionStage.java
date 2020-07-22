5303db02b5ce5e063b3d9bf9c510407c2478ec98
mehdi <mf1378mf@yahoo.com>
2020-07-22 12:43:09 +0430

    yeh

--- src/main/java/mipsim/pipeline/stages/ExecutionStage.java
+++ src/main/java/mipsim/pipeline/stages/ExecutionStage.java
@@ -6,6 +6,8 @@ import mipsim.units.ForwardingUnit;
 import mipsim.module.Multiplexer;
 import sim.base.BusKt;
 
+import static sim.gates.GatesKt.or;
+
 public class ExecutionStage extends Stage {
 	public ExecutionStage(final Processor processor) {
 		super(processor);
@@ -30,21 +32,22 @@ public class ExecutionStage extends Stage {
 		//todo Ask friends
 
 		var resultOneOfAlu = BusKt.bus(32);
-		var forwarding1 = BusKt.bus(2);
-		ForwardingUnit.forwardingUnitEXHazard(exmem.regWrite, exmem.rtRegister, idex.rsRegister, forwarding1);
+		var forwardingEx1 = BusKt.bus(2);
+		var forwardingMem1 = BusKt.bus(2);
+		ForwardingUnit.forwardingUnitEXHazard(exmem.regWrite,exmem.rtRegister, idex.rsRegister, forwardingEx1);
+		ForwardingUnit.forwardingUnitMEMHazard(memwb.regWrite,memwb.rdRegister,idex.rsRegister,exmem.regWrite,exmem.rtRegister,forwardingMem1);
 
+		Multiplexer.aluInput(or(forwardingEx1,forwardingMem1), idex.rsData, exmem.aluData, memwb.aluData, resultOneOfAlu);
 
-		Multiplexer.aluInput(forwarding1, idex.rsData, exmem.aluData, memwb.aluData, resultOneOfAlu);
-		// code above have can not detective load hazard detection
 		var resultTowOfAlu = BusKt.bus(32);
-		var forwarding2 = BusKt.bus(2);
-
+		var forwardingExe2 = BusKt.bus(2);
+		var forwardingMem2 = BusKt.bus(2);
 		var resAluSrc = BusKt.bus(32);
 		Multiplexer.aluSrc(idex.aluSrc, idex.rtData, idex.immediate, resAluSrc);
-		ForwardingUnit.forwardingUnitEXHazard(exmem.regWrite, exmem.rtRegister, idex.rtRegister, forwarding2);
+		ForwardingUnit.forwardingUnitEXHazard(exmem.regWrite, exmem.rtRegister, idex.rtRegister, forwardingExe2);
+		ForwardingUnit.forwardingUnitMEMHazard(memwb.regWrite,memwb.rdRegister,idex.rsRegister,exmem.regWrite,exmem.rtRegister,forwardingMem2);
 
-
-		Multiplexer.aluInput(forwarding2, resAluSrc, exmem.aluData, memwb.aluData, resultOneOfAlu);
+		Multiplexer.aluInput(or(forwardingEx1,forwardingMem1), idex.rsData, exmem.aluData, memwb.aluData, resultTowOfAlu);
 		LogicALU.AluInStage(resultOneOfAlu, resultTowOfAlu, idex.function, idex.aluOp, exmem.aluData);
 
 		/* note:
