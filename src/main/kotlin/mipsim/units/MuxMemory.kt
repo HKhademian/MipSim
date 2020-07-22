80861884f514339106bc3eac85833d2d95b1842c
Hossain Khademian <hossain.khademian@gmail.com>
2020-07-22 19:39:47 +0430

    begin mux mem idea
    for project presentation

--- src/main/kotlin/mipsim/units/MuxMemory.kt
+++ src/main/kotlin/mipsim/units/MuxMemory.kt
@@ -9,6 +9,6 @@ open class MuxMemory protected constructor(addressBusSize: Int) {
 	protected val wordCount: Int = 1 shl addressBusSize
 	protected val memory: Memory = createWords(wordCount)
 	protected val words: List<MutableBus> = (0 until wordCount).map { memory.getWord(it) }
-	val address: MutableBus = bus(wordCount)
+	val address: MutableBus = bus(addressBusSize)
 	val word: Bus = mux(address, words)
 }
