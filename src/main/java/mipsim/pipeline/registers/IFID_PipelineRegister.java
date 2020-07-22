de6f71ce56a65519ad5c63becab2cd4c37c3e5ae
Hossain Khademian <hossain.khademian@gmail.com>
2020-07-22 18:44:40 +0430

    PipelineRegister DebugWriter

--- src/main/java/mipsim/pipeline/registers/IFID_PipelineRegister.java
+++ src/main/java/mipsim/pipeline/registers/IFID_PipelineRegister.java
@@ -1,9 +1,12 @@
 package mipsim.pipeline.registers;
 
+import org.jetbrains.annotations.NotNull;
 import sim.base.BusKt;
 import sim.base.MutableValue;
 
 import java.util.List;
+
+import static mipsim.sim.InstructionParserKt.parseBinToInstruction;
 
 public final class IFID_PipelineRegister extends PipelineRegister {
 	public final List<MutableValue> pc = BusKt.slice(memory, 0, 32);//this will be 32 bit for the branch and jump
@@ -14,5 +17,15 @@ public final class IFID_PipelineRegister extends PipelineRegister {
 
 	public IFID_PipelineRegister() {
 		super(65);
+	}
+
+	@Override
+	public void writeDebug(@NotNull StringBuffer buffer) {
+		var pc = BusKt.toInt(this.pc);
+		var instructionBin = BusKt.toInt(this.instruction);
+		var instructionStr = parseBinToInstruction(instructionBin);
+		buffer
+			.append(String.format("PC: %08xH\t", pc))
+			.append(String.format("INST: %08xH = ' %s '\t", instructionBin, instructionStr));
 	}
 }
--- src/main/java/mipsim/pipeline/registers/PipelineRegister.java
+++ src/main/java/mipsim/pipeline/registers/PipelineRegister.java
@@ -2,12 +2,14 @@ package mipsim.pipeline.registers;
 
 import mipsim.units.Memory;
 import mipsim.units.MemoryKt;
+import org.jetbrains.annotations.NotNull;
+import sim.DebugWriter;
 import sim.base.Eval;
 
 /**
  * all pipeline registers use this
  */
-public abstract class PipelineRegister implements Eval {
+public abstract class PipelineRegister implements Eval, DebugWriter {
 	protected final Memory memory;
 
 	protected PipelineRegister(int bitSize) {
@@ -17,5 +19,17 @@ public abstract class PipelineRegister implements Eval {
 	@Override
 	public void eval() {
 		memory.eval();
+	}
+
+	@Override
+	public void writeDebug(@NotNull StringBuffer buffer) {
+		memory.writeDebug(buffer);
+	}
+
+	@Override
+	public String toString() {
+		var buffer = new StringBuffer();
+		writeDebug(buffer);
+		return buffer.toString();
 	}
 }
