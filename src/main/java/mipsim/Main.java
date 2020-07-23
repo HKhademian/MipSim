8a86def250e4bafffd1f22368f1b1e07fada87f4
Hossain Khademian <hossain.khademian@gmail.com>
2020-07-23 22:55:12 +0430

    better debug idea

--- src/main/java/mipsim/Main.java
+++ src/main/java/mipsim/Main.java
@@ -5,6 +5,8 @@ import mipsim.sim.InstructionParserKt;
 import java.util.Arrays;
 import java.util.List;
 
+import static sim.test.TestKt.testOn;
+
 public class Main {
 	static List<String> instructions = Arrays.asList(
 		"add $t0, $t1, $t2",
@@ -13,14 +15,32 @@ public class Main {
 	);
 
 	public static void main(String[] args) {
+		List<String> instructions = Arrays.asList(
+			"add $t0, $t1, $t2",
+			"add $t3, $t0, $t2",
+			"add $t4, $t0, $t2"
+		);
+
 		final var processor = new Processor();
 		processor.init();
-
 		InstructionParserKt.loadInstructions(processor, instructions);
+		System.out.println(processor.instructionMemory._memory);
 
-		processor.eval(System.nanoTime());
-		processor.eval(System.nanoTime());
-		processor.eval(System.nanoTime());
-		processor.eval(System.nanoTime());
+		testOn(null, "clock 1", () -> {
+			final var time = System.nanoTime();
+
+			processor.pc.eval(time);
+			processor.ifStage.eval(time);
+			processor.ifid.eval(time);
+		});
+
+		testOn(null, "clock 2", () -> {
+			processor.eval(System.nanoTime());
+		});
+
+		testOn(null, "clock 3", () -> {
+			processor.eval(System.nanoTime());
+		});
+
 	}
 }
