6f0cc2f3e110063a885f0063188c3f01dc28a0d1
mehdi <mf1378mf@yahoo.com>
2020-07-26 20:46:39 +0430

    make test harder

--- src/main/java/mipsim/Test.java
+++ src/main/java/mipsim/Test.java
@@ -2,15 +2,18 @@ package mipsim;
 
 import kotlin.io.FilesKt;
 import kotlin.text.Charsets;
+import kotlinx.coroutines.flow.internal.NopCollector;
 
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+
 import static mipsim.sim.ParserKt.HALT;
 import static mipsim.sim.ParserKt.NOP;
 import static sim.tool.TestKt.testOn;
+
 
 public class Test {
 	public static void main(String... args) {
@@ -23,7 +26,7 @@ public class Test {
 				"3.Time to shift your amount\n" +
 				"4.Do you like to see how can I jump to any Address you want\n5.If condition be good I can come back I have Branch backward and forward\n" +
 				"6.Time to check set less than\n" +
-				"7.Okey I'm good horse you can run any program that you like to run please Entere name of your file\n" +
+				"7.Okey I'm good horse you can run any program that you like to run please Enter name of your file\n" +
 				"\n8.Exit.");
 			int choice = myObj.nextInt();
 			switch (choice) {
@@ -93,6 +96,9 @@ public class Test {
 			"sub  $3,$3,$2",
 			"and  $zero,$3,$1",
 			NOP,
+			NOP,
+			NOP,
+			NOP,
 			HALT
 		);
 	}
@@ -103,6 +109,9 @@ public class Test {
 			"sw $1,4($1)",
 			"lw $2,4($1)",
 			"addi  $3,$2,3",
+			NOP,
+			NOP,
+			NOP,
 			NOP,
 			HALT
 		);
@@ -115,6 +124,9 @@ public class Test {
 			"sll  $4,$1,10",
 			"srl  $3,$2,2",
 			NOP,
+			NOP,
+			NOP,
+			NOP,
 			HALT
 		);
 	}
@@ -123,10 +135,13 @@ public class Test {
 		testCase(
 			"addi $1,$0,10",
 			"addi $2,$0,2",
-			"j 5",
+			"j 7",
 			NOP,
 			"add  $5,$1,$2 ",
 			"add  $6,$1,$2",
+			NOP,
+			NOP,
+			NOP,
 			NOP,
 			HALT
 		);
@@ -135,12 +150,13 @@ public class Test {
 	public static void testFive() {
 		testCase(
 			"addi $3,$zero,4",
-			"beq  $3,$3,3",
-			NOP,
-			NOP,
 			"addi $2,$zero,4",
 			"addi $4,$zero,4",
-			"beq  $4,$2,-2",
+			"beq  $3,$zero,5",
+			"addi $3,$3,-1",
+			"beq  $4,$2,-5",
+			NOP,
+			NOP,
 			NOP,
 			NOP,
 			HALT
@@ -150,14 +166,14 @@ public class Test {
 
 	public static void testSix() {
 		testCase(
+			"addi $7,$zero,1",
 			"addi $1,$zero,13",
 			"addi $2,$zero,10",
 			"addi $1,$1,-1",
 			"slt  $3,$1,$2",
-			"beq  $3,$zero,-3",
+			"beq  $3,$7,4",
+			"j 3",
 			NOP,
-			NOP,
-			"j 0",
 			NOP,
 			NOP,
 			NOP,
