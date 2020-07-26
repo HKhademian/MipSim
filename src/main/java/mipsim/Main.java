ffcb4e8d4698388c798a6051b8c127291839e041
ramin <raminrowshan153@gmail.com>
2020-07-26 12:21:48 +0430

    this is mehdi fghieh push

--- src/main/java/mipsim/Main.java
+++ src/main/java/mipsim/Main.java
@@ -4,27 +4,58 @@ import mipsim.sim.ParserKt;
 import sim.tool.DebugKt;
 
 import java.io.File;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import static sim.tool.TestKt.testOn;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Please enter mips inst file to run:");
-		final var fileName = new Scanner(System.in).nextLine();
-		final var file = new File(fileName);
 
-		final var processor = new Processor();
-		processor.init();
-		ParserKt.loadInstructions(processor.instructionMemory._memory, file, false);
-		DebugKt.println(processor.instructionMemory._memory);
 
-		for (var i = 0; i < 20; i++) {
-			testOn(processor, "clock " + i, () -> {
-				processor.eval(System.nanoTime());
-			});
+	public static void main(String[] args){
+		int i = 0;
+		boolean contuie = true;
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		System.out.println("\n\nHi Lovely\nWelcome to my cpu simulator.");
+		while (contuie) {
+			System.out.println("\n\nDo you like to test it's power.?\n1.I like to see rType function with" +
+				" hazard\n2.Do you like to see how can I sw and lw in memory and I detect Lw hazard for you\n" +
+				"3.Time to shift your amount\n" +
+				"4.Do you like to see how can I jump to any Address you want\n5.If condition be good I can come back I have Branch backward and forward\n" +
+				"6.Time to check set less than\n" +
+				"7.Okey I'm good horse you can run any program that you like to run please Entere name of your file\n" +
+				"\n8.Exit.");
+			String choice = myObj.nextLine();
+			switch (Integer.valueOf(choice)) {
+
+				case 1:
+					TestMain.testOne();
+					break;
+				case 2:
+					TestMain.testTwo();
+					break;
+				case 3:
+					TestMain.testTree();
+					break;
+				case 4:
+					TestMain.testFour();
+					break;
+				case 5:
+					TestMain.testFive();
+					break;
+				case 6:
+					TestMain.testSix();
+					break;
+				case 7:
+					TestMain.readFile();
+					break;
+				case 8:
+					contuie = false;
+					break;
+			}
 		}
-
-		testOn(processor.dataMemory._memory, "dataMemory");
 	}
 }
+
---
+++ src/main/java/mipsim/TestMain.java
@@ -0,0 +1,209 @@
+package mipsim;
+
+import mipsim.sim.ParserKt;
+import sim.tool.DebugKt;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static mipsim.sim.ParserKt.NOP;
+import static sim.tool.TestKt.testOn;
+
+public class TestMain {
+	  // Create a Scanner object
+
+	/**
+	 * in this test we check hazard detection And this fact we can't write in zero register
+	 */
+	public static void testOne(){
+		List<String> instructions = Arrays.asList(
+			"addi $1,$zero,10",
+			"addi $2,$zero,13",
+			"add  $3,$2,$1 ",
+			"sub  $3,$3,$2",
+			"and  $zero,$3,$1"
+		);
+		final var processor = new Processor();
+		processor.init();
+		ParserKt.loadInstructions(processor, instructions, false);
+		DebugKt.println(processor.instructionMemory._memory);
+
+		var clockContinue = true;
+		int i = 0;
+		while (clockContinue) {
+			testOn(processor, "clock " + i, () -> {
+				processor.eval(System.nanoTime());
+			});
+			i++;
+			System.out.println("\n Do you like to see next clock cycle ? Y/N");
+			clockContinue = new Scanner(System.in).nextLine().equals("Y");
+
+		}
+
+		testOn(processor.dataMemory, "dataMemory");
+	}
+
+	public static void testTwo() {
+		List<String> instructions = Arrays.asList(
+			"addi $1,$zero,4",
+			"$sw $1,4($1)",
+			"$lw $2,4($1)",
+			"addi  $3,$2,3"
+		);
+		final var processor = new Processor();
+		processor.init();
+		ParserKt.loadInstructions(processor, instructions, false);
+		DebugKt.println(processor.instructionMemory._memory);
+
+		var clockContinue = true;
+		int i = 0;
+		while (clockContinue) {
+			testOn(processor, "clock " + i, () -> {
+				processor.eval(System.nanoTime());
+			});
+			i++;
+			System.out.println("\n Do you like to see next clock cycle ? Y/N");
+			clockContinue = new Scanner(System.in).nextLine().equals("Y");
+
+		}
+
+		testOn(processor.dataMemory, "dataMemory");
+
+	}
+
+	public static void testTree() {
+		List<String> instructions = Arrays.asList(
+			"addi $1,$zero,10",
+			"addi $2,$zero,13",
+			"sll  ٫$4,$1,10",
+			"srl  $3,$3,$2"
+
+		);
+		final var processor = new Processor();
+		processor.init();
+		ParserKt.loadInstructions(processor, instructions, false);
+		DebugKt.println(processor.instructionMemory._memory);
+
+		var clockContinue = true;
+		int i = 0;
+		while (clockContinue) {
+			testOn(processor, "clock " + i, () -> {
+				processor.eval(System.nanoTime());
+			});
+			i++;
+			System.out.println("\n Do you like to see next clock cycle ? Y/N");
+			clockContinue = new Scanner(System.in).nextLine().equals("Y");
+
+		}
+
+		testOn(processor.dataMemory, "dataMemory");
+	}
+
+	public static void testFour() {
+		List<String> instructions = Arrays.asList(
+			"addi $1,$zero,10",
+			"addi $1.$zero,2",
+			"j 2",
+			"add  $3,$2,$1 ",
+			"sub  $3,$3,$2",
+			"and  $3,$3,$1"
+		);
+		final var processor = new Processor();
+		processor.init();
+		ParserKt.loadInstructions(processor, instructions, false);
+		DebugKt.println(processor.instructionMemory._memory);
+
+		var clockContinue = true;
+		int i = 0;
+		while (clockContinue) {
+			testOn(processor, "clock " + i, () -> {
+				processor.eval(System.nanoTime());
+			});
+			i++;
+			System.out.println("\n Do you like to see next clock cycle ? Y/N");
+			clockContinue = new Scanner(System.in).nextLine().equals("Y");
+
+		}
+
+		testOn(processor.dataMemory, "dataMemory");
+	}
+
+	public static void testFive() {
+		List<String> instructions = Arrays.asList(
+			"addi $3,$zero,4",
+			"beq  $3,$3,5",
+			"addi $2,$zero,4",
+			"addi $4,$zero,4",
+			"beq  $4,$2,-2",
+			NOP
+
+		);
+		final var processor = new Processor();
+		processor.init();
+		ParserKt.loadInstructions(processor, instructions, false);
+		DebugKt.println(processor.instructionMemory._memory);
+
+		var clockContinue = true;
+		int i = 0;
+		while (clockContinue) {
+			testOn(processor, "clock " + i, () -> {
+				processor.eval(System.nanoTime());
+			});
+			i++;
+			System.out.println("\n Do you like to see next clock cycle ? Y/N");
+			clockContinue = new Scanner(System.in).nextLine().equals("Y");
+
+		}
+
+		testOn(processor.dataMemory, "dataMemory");
+	}
+
+	public static void testSix() {
+		List<String> instructions = Arrays.asList(
+			"addi $1,$zero,13",
+			"addi $2,$zero,10",
+			"slt  $3,$1,$2",
+			"addi $1,$1,-1",
+			"beq  $3,$zero,-1",
+			"jmp 0",
+			NOP,
+			NOP
+
+		);
+		final var processor = new Processor();
+		processor.init();
+		ParserKt.loadInstructions(processor, instructions, false);
+		DebugKt.println(processor.instructionMemory._memory);
+
+		var clockContinue = true;
+		int i = 0;
+		while (clockContinue) {
+			testOn(processor, "clock " + i, () -> {
+				processor.eval(System.nanoTime());
+			});
+			i++;
+			System.out.println("\n Do you like to see next clock cycle ? Y/N");
+			clockContinue = new Scanner(System.in).nextLine().equals("Y");
+
+		}
+
+		testOn(processor.dataMemory, "dataMemory");
+	}
+
+	public static void readFile() {
+		System.out.println("Please enter mips inst file to run:");
+		final var fileName = new Scanner(System.in).nextLine();
+		final var file = new File(fileName);
+
+		final var processor = new Processor();
+		processor.init();
+		ParserKt.loadInstructions(processor.instructionMemory._memory, file, false);
+		DebugKt.println(processor.instructionMemory._memory);
+	}
+}
+
+
+
+
