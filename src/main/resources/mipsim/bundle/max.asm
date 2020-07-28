14e6161097e17b164b1d09dbfca4063dc156b95c
ramin <raminrowshan153@gmail.com>
2020-07-28 21:13:58 +0430

    fix file

--- src/main/resources/mipsim/bundle/max.asm
+++ src/main/resources/mipsim/bundle/max.asm
@@ -1,15 +1,21 @@
 addi $1,$zero,4  ;array address
-lw $2,0($1)			 ;
+lw $4,0($1)			 ;
 addi $1,$1,4
 lw $3,0($1)
-addi $4,$zero,1
-beq $4,$2,8
+addi $2,$zero,1
+beq $4,$2,14
+NOP
+NOP
 addi $1,$1,4
-lw $4,0($1)
-slt $7,$3,$4
-beq $7,$zero,1
-addi $3,$4,0
+lw $6,0($1)
+slt $7,$3,$6
+beq $7,$zero,3
+NOP
+NOP
+addi $3,$6,0
 addi $2,$2,1
+NOP
+NOP
 j 6
 NOP
 NOP
--- src/main/resources/mipsim/bundle/sum.asm
+++ src/main/resources/mipsim/bundle/sum.asm
@@ -3,10 +3,16 @@ lw $s1, 0($s0)				; size:$s1 = *array
 addi $s0, $s0, 4			; array+=4
 lw $t2, 0($s0)				; sum:$t2 = *array
 addi $t0, $zero, 1		; i:$t0=1
-beq $t0, $s1, 5				; --jmp=6*4=24 -> if(i==size) jumpTo end
+beq $t0, $s1, 9				; --jmp=6*4=24 -> if(i==size) jumpTo end
+NOP
+NOP
 addi $s0,$s0,4				; array+=4
+NOP
+NOP
 lw $t1,0($s0)					; tmp:$t1=*array
 add $t2,$t2,$t1				; sum+=$5
 addi $t0,$t0,1				; i+=1
-j 24									; jumpTo jmp
+j 6									; jumpTo jmp
 NOP										; --end=12*4=48--;
+NOP
+HALT
